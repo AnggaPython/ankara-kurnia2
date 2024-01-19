67f754e492b332c073ebd27bb873ed107a3286fd
Ankara Kurnia <88472694+AnggaPython@users.noreply.github.com>
2024-01-19 21:49:34 +0700

    add new file

---
+++ utils/techStack.ts
@@ -0,0 +1,15 @@
+const techStack = [
+  "reactjs",
+  "nodejs",
+  "nextjs",
+  "javaScript",
+  "python",
+  "mongodb",
+  "typescript",
+  "expressjs",
+  "firebase",
+  "tailwindcss",
+  "angular"
+];
+
+export default techStack;
